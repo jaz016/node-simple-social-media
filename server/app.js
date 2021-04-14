203ad3efe6b5273286ec3f2ff9b0838cc9e26f73
Jasper Bautista <xjet16@gmail.com>
2021-04-14 11:03:11 +0800

    initial commit

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+
+
+app.get('/', (req, res) => {
+	res.send("Hello world~");
+});
+
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server now listening at port ${process.env.PORT}..`);
+})
